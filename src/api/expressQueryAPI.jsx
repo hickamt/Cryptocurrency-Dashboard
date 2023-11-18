45d6fd8746d238c5eef83c16f4f848fa92c47505
todd <thickam@outlook.com>
2023-11-18 00:57:14 -0800

    second and third and ... save

--- src/api/expressQueryAPI.jsx
+++ src/api/expressQueryAPI.jsx
@@ -1,7 +1,6 @@
 import remaining from "../../public/remaining.json";
 
 export default async function expressQueryAPI(query = "remaining") {
-  console.log("remaining: ", remaining);
   try {
     return await fetch("../../public/remaining.json")
       .then(async (res) => {
@@ -14,6 +13,7 @@ export default async function expressQueryAPI(query = "remaining") {
         console.error(`Error: unable to fetch ${query} from server`, error);
       });
   } catch (error) {
+    return remaining;
     console.error(error.message);
   }
 }
